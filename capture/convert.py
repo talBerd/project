0b2314b739a7e18daf42a2844c87df60d3874442
talBerd <102621396+talBerd@users.noreply.github.com>
2025-05-25 17:59:02 +0300

    Update convert.py

--- capture/convert.py
+++ capture/convert.py
@@ -17,7 +17,7 @@ def convert_pcaps(parallel_processes=5):
 
         for pcap in batch:
             category, website = pcap.split(os.path.sep)[-3:-1]
-            if category == "big_file":
+            if category == "big_file" or category == "game":
                 continue
             csv_dir = os.path.join(csv_base, category, website)
             os.makedirs(csv_dir, exist_ok=True)
